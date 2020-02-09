b1845b2f5001d76863f4785b35007a20276b8adf
indro8307 <indra_neel83@yahoo.co.in>
2020-02-09 11:24:44 +0530

    Create swapCases.py

---
+++ Python/swapCases.py
@@ -0,0 +1,9 @@
+def swap_case(s):
+    for i in s:
+        if((ord(i) > 64) and (ord(i) < 91)):
+            print(chr(ord(i) + 32),end="")
+        elif((ord(i) > 96) and (ord(i) < 123)):
+            print(chr(ord(i) - 32),end="")
+        else:
+            print(i,end="")
+    return '\n'
