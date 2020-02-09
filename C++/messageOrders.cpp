ae8f669a824fe1cd7717365af0c3317d8b48bfd2
indro8307 <indra_neel83@yahoo.co.in>
2020-02-09 11:20:03 +0530

    Create messageOrders.cpp

---
+++ C++/messageOrders.cpp
@@ -0,0 +1,31 @@
+
+
+class Message {
+    string test;
+    int seq;
+public: 
+    Message() {}
+    Message(string a, int b){
+        test = a;
+        seq = b;
+    }
+
+    const string& get_text() {
+        return test;
+    }
+
+    bool operator<(const Message &other) const {
+            return seq < other.seq;
+    }
+};
+
+class MessageFactory {
+    int count;
+public:
+    MessageFactory():count(0) {}
+    Message create_message(const string& text) {
+        count++;
+        return Message(text,count);
+    }
+};
+
