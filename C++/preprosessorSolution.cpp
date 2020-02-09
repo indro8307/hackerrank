7174a228f2849e5211e45c0e9b831deb1b06af73
indro8307 <indra_neel83@yahoo.co.in>
2020-02-09 11:17:11 +0530

    Create preprosessorSolution.cpp

---
+++ C++/preprosessorSolution.cpp
@@ -0,0 +1,13 @@
+/* Enter your macros here */
+#define INF 100000000
+#define FUNCTION(arg1, op)    \
+void arg1(int &a, int &b){   \
+    if(b op a){ \
+        a = b;  \
+    }   \
+}   \
+
+#define toStr(x) #x
+#define foreach(v, i) for(int i=0;i<v.size();i++)
+#define io(v) cin>>v
+
