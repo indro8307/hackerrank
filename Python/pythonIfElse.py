8d4fa48b2142133a818a4dbe74c9d57448992478
indro8307 <indra_neel83@yahoo.co.in>
2020-02-09 11:29:39 +0530

    Create pythonIfElse.py

---
+++ Python/pythonIfElse.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if (N%2 == 1):
+        print('Weird')
+    elif (N%2 == 0):
+        if(N>=2):
+            if(N<=5):
+                print('Not Weird')
+        if(N>=6):
+            if(N<=20):
+                print('Weird')
+        if(N>20):
+            print('Not Weird')
+        
+     
+       
